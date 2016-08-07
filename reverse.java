c848858b95d0f8b5d9f8b990da03798856f7a974
prabaprabakaran <prabakar.431@gmail.com>
2016-08-06 22:09:08 -0700

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,14 @@
+
+public class rev {
+
+
+	public static void main(String[] args) {
+		String se="java";
+		StringBuffer s=new StringBuffer(se);
+		s.reverse();
+		System.out.println(s);
+		
+
+	}
+
+}
